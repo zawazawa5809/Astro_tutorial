3e2eb6a77798c6b5cd2e24a8ebca30fc9b56dacf
zawa <kakizsawa5809@gmail.com>
2024-04-20 14:02:29 +0000

    "edit astro.config.mjs"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zawazawa5809.github.io",
-  base: "/Astro_tutorial",
+  // base: "/Astro_tutorial",
   integrations: [tailwind()],
 });
