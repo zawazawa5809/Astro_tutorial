ee5bab9143f850fe071cceba8561ed4a95bb844c
zawa <kakizsawa5809@gmail.com>
2024-04-21 02:12:33 +0000

    delete Base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://zawazawa5809.github.io",
-  // base: "/Astro_tutorial",
-  base: "https://github.com/zawazawa5809/Astro_tutorial",
   integrations: [tailwind()],
 });
