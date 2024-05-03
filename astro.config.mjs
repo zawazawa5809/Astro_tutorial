1dd4d20b4699bece78c038f57f72684b64ce34aa
zawa <kakizsawa5809@gmail.com>
2024-05-03 09:02:07 +0000

    Finish tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://zawazawa5809.github.io/",
   base: "/Astro_tutorial",
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true
+      }
+    }
+  }
 });
